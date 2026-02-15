0eb160aec606739606196d9b839a3d4eb4bc9c96
Max Baker <crimsonender99@gmail.com>
2026-02-15 12:36:24 -0800

    removed the leaderboard packages that won't work

---
+++ Assets/Scripts/Menus/Leaderboard.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Leaderboard: MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> names;
+    [SerializeField] private List<TextMeshProUGUI> scores;
+}
