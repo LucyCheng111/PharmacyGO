d89588395a743ce4ed6feaf8c64780ed2ffa4c9f
Nick Shininger <shiningn@oregonstate.edu>
2026-05-05 16:40:54 -0700

    fixed bug for always recognizing mm as free

--- Assets/Scripts/Player/NPCController.cs
+++ Assets/Scripts/Player/NPCController.cs
@@ -72,8 +72,11 @@ public class NPCController : MonoBehaviour, Interactable
             {
                 if (ProgressionState.Instance.HasItem(sceneTransitioner.NecessaryProgressionItemName))
                 {
-                    if(sceneTransitioner.levelNumber == -1 && convertSpawnPointIDtoLevel(sceneTransitioner.targetSpawnPointID) < PlayerPrefs.GetInt("UnlockedLevel"))
+                    int indexOfLevel = PlayerPrefs.GetInt("UnlockedLevel");
+
+                    if(sceneTransitioner.levelNumber == -1 && convertSpawnPointIDtoLevel(sceneTransitioner.targetSpawnPointID) < LevelManager.Instance.GetLevelNumberFromIndex(indexOfLevel))
                     {
+                        Debug.Log("TEST: " + LevelManager.Instance.GetLevelNumberFromIndex(indexOfLevel));
                         //player has gotten past this MM level
                         willCharge = false;
                         StartCoroutine(DialogManager.Instance.ShowDialog(TravelIsFreeDialog, travelChoices, OnTravelChoiceSelected));
